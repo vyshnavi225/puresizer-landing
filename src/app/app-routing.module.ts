7e9ba7d443fe5cd9eb24c3e7f16f0b42519e428d
Sonika-dot-HP <Sonika.HP@Maplelabs.Com>
2022-01-24 13:33:55 +0530

    Revert "UI: landing page of pure lander and ui setup"
    This reverts commit c3dd6cc51536df6064bc18e73ad147c2d3d1cbb5.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LandingComponent } from './landing/landing.component';
 
-const routes: Routes = [
-  { path: 'landing', component: LandingComponent },
-  // {
-  //   path: 'scenario',
-  //   component: ScenariosComponent,
-  //   data: {
-  //     viewName: 'scenario'
-  //   }
-  // },
-  {
-    path: '**',
-    redirectTo: '/landing'
-  },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,18 @@
-import { APP_BASE_HREF } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ApplicationDataService, APP_DATA_KEYS } from 'src/shared/services/application-data.service';
-import { HttpServicesModule } from 'src/shared/services/http/http-services.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LandingComponent } from './landing/landing.component';
-import { AuthenticationService } from './service/authentication.service';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LandingComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpServicesModule,
+    AppRoutingModule
   ],
-  providers: [
-    AuthenticationService,
-    ApplicationDataService,
-    { provide: APP_BASE_HREF, useValue: `/${APP_DATA_KEYS.BASE_HREF}/` },
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
